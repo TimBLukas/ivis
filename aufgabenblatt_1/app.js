e4faa50ac1263c4d2c11e9b78eccff81c12c3942
Tim Lukas <timlukas@Mac.fritz.box>
2025-10-19 10:37:58 +0200

    update

---
+++ aufgabenblatt_1/app.js
@@ -0,0 +1,4 @@
+
+d3.csv("testdataemployee.csv").then(data, () => {
+    console.log(data);
+})
---
+++ tutorial_0/main.js
@@ -0,0 +1,10 @@
+const svgWidth = 800;
+const svgHeight = 300;
+
+let svg = d3.select("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("class", "chart")
+
+
+            
