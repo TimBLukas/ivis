71422544a4efe1e8587b37f8604cedce3b7d94b0
Tim Lukas <timlukas@MacBook-Pro-von-Tim-3.local>
2025-10-30 17:43:22 +0100

    Kommentare ergänzt

--- aufgabenblatt_2/aufgabe_3/main.js
+++ aufgabenblatt_2/aufgabe_3/main.js
@@ -1,3 +1,10 @@
+/**
+ * Konvertiert die Bevölkerungsanzahl
+ *  von: 1B, 12M, 123K 
+ *  zu: 1_000_000_000, 12_000_000, 123_000
+ * @param num Bevölkerungsanzahl die konvertiert werden soll 
+ * @returns Konvertierte Bevölkerungszahl als Float
+ */
 function parsePopulation(num) {
   var lastChar = num.slice(-1);
   if (lastChar == "B") {
@@ -11,11 +18,16 @@ function parsePopulation(num) {
   }
 }
 
+// Visualisierung erstellen
 d3.csv("gapminder.csv").then(
+  /**
+   * 
+   * @param dataset Datenset, das in den Graph konvertiert werden soll
+   */
   function (dataset) {
-    // Konstanten für Größen Angaben definierej
-    const svgWidth = 700;
-    const svgHeight = 500;
+    // Konstanten für Größe und Margins
+    const svgWidth = 900;
+    const svgHeight = 600;
     const margin = {
       top: 20,
       right: 30,
@@ -28,6 +40,7 @@ d3.csv("gapminder.csv").then(
     let rParam = "Population";
     let colorParam = "world_4region";
 
+    // Extrema der festgelegten Paramter bestimmen bestimmen
     let minX = d3.min(dataset, (d) => parseFloat(d[xParam]));
     let maxX = d3.max(dataset, (d) => parseFloat(d[xParam]));
 
@@ -36,7 +49,8 @@ d3.csv("gapminder.csv").then(
 
     let minR = d3.min(dataset, (d) => parsePopulation(d[rParam]));
     let maxR = d3.max(dataset, (d) => parsePopulation(d[rParam]));
-    // Farbscale für die Bubbles basierend auf den Spalte world_4region
+
+    // Farbskala basierend auf den in der Spalte world_4region enthaltenen Möglichkeiten
     let colorInt = d3
       .scaleOrdinal()
       .domain(["asia", "europe", "africa", "americas"])
@@ -54,7 +68,7 @@ d3.csv("gapminder.csv").then(
     // Y-Skala basierend auf dem ermittelten minY und maxY Wert
     let scaleY = d3.scaleLinear().domain([minY, maxY]).range([svgHeight, 0]);
 
-    let scaleR = d3.scaleSqrt().domain([minR, maxR]).range([3, 55]);
+    let scaleR = d3.scaleSqrt().domain([minR, maxR]).range([5, 55]);
 
     // Skalen zur SVG hinzufügen
     svg
@@ -89,18 +103,20 @@ d3.csv("gapminder.csv").then(
       .style("font-size", "14px")
       .text(yParam);
 
+    // Tooltip für das Hovern über den Bubbles
     var tooltip = d3
       .select("#visualisation-container")
-      .append("div") // <-- ÄNDERUNG: div statt rect
-      .attr("class", "tooltip") // Gib ihm eine Klasse zum Stylen
-      .style("opacity", 0) // Starte unsichtbar
-      .style("position", "absolute") // Wichtig, damit wir es frei positionieren können
+      .append("div") 
+      .attr("class", "tooltip") 
+      .style("opacity", 0) 
+      .style("position", "absolute") 
       .style("background-color", "white")
       .style("border-radius", "10px")
       .style("padding", "10px")
       .style("color", "black")
       .style("pointer-events", "none");
 
+    // Funktion, die bei mouse over getriggert wird und das div (Tooltip) anzeigt
     let showTooltip = function (event, d) {
       tooltip.transition().duration(200);
       tooltip
@@ -117,18 +133,19 @@ d3.csv("gapminder.csv").then(
         .style("left", event.pageX + 30 + "px")
         .style("top", event.pageY + 30 + "px");
     };
-
+    // Bei mouse-move aufgerufen
     let moveTooltip = function (event, d) {
       tooltip
         .style("left", event.pageX + 30 + "px")
         .style("top", event.pageY + 30 + "px");
     };
 
+    // Wird bei mouse-out aufgerufen
     let hideTooltip = function (d) {
       tooltip.transition().duration(200).style("opacity", 0);
     };
 
-    // Bubbles für die Länder erstellen
+    // Bubbles erstellen (1 pro Land)
     let chart = svg
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -156,7 +173,7 @@ d3.csv("gapminder.csv").then(
       { id: "africa", label: "Afrika", value: "africa" },
     ];
 
-    // Alle kontinente, die in der Visualisierung gezeigt werden sollen sammeln (Am Anfang alle Kontinente)
+    // Alle Kontinente, die in der Visualisierung gezeigt (Checkbox gechecked) werden sollen sammeln (Am Anfang alle Kontinente)
     let activeFilters = new Set(continents.map((c) => c.value));
 
     // Control Container erstellen
@@ -179,7 +196,7 @@ d3.csv("gapminder.csv").then(
         .attr("value", continent.value)
         .style("margin-right", "5px")
         .style("height", "15px")
-        .property("checked", true) // Anfangs alle aktiviert
+        .property("checked", true) // Default = aktiv
         .on("change", function () {
           // Filter aktualisieren (Unterscheidung ob checkbox aktiviert oder dekativiert wurde)
           if (this.checked) {
@@ -190,13 +207,14 @@ d3.csv("gapminder.csv").then(
           updateChart();
         });
 
+      // Beschreibung zur Checkbox ergänzen
       checkboxWrapper
         .append("label")
         .attr("for", `${continent.id}-checkbox`)
         .text(continent.label);
     });
 
-    // Funktion zum Aktualisieren des Charts
+    // chart nach Änderungen durch Checkbox aktualisieren
     function updateChart() {
       svg
         .selectAll(".bubbles")
